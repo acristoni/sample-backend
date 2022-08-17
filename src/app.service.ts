8b9f84ec981da052e79c814ea1899e18dc9255de
acristoni <adonai_cn@hotmail.com>
2022-08-17 14:28:51 -0300

    create user controller, service & event

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserRequest } from './create-user-request.dto';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
+  private readonly users: any[] = []
+  createUserRequest: any;
+
+  constructor(
+    @Inject('COMMUNICATION') private readonly communicationClient: ClientProxy,
+  ) {}
+  
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser(createUserRequest: CreateUserRequest) {
+    this.users.push(createUserRequest);
+    this.communicationClient.emit(
+      'user_created', 
+      new CreateUserEvent(createUserRequest.email),
+    );
   }
 }
