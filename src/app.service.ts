f1b59646b4e7c385ee05c85c4ac48df48e366bdb
acristoni <adonai_cn@hotmail.com>
2022-08-17 15:07:01 -0300

    fix port

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,7 +13,7 @@ export class AppService {
   ) {}
   
   getHello(): string {
-    return 'Hello World!';
+    return 'I am Gateway microservice!';
   }
 
   createUser(createUserRequest: CreateUserRequest) {
